646ad05e0308eab9e69e0794dcb029dc1b6eeecf
bestsuperweb <bestsuperweb@gmail.com>
2018-05-07 00:03:43 -0400

    update after signout path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # protect_from_forgery with: :null_session
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
