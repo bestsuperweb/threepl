e2d96600e29aa915223ffbf2eabfff8961277d6c
bestsuperweb <coolplum@coolplums-iMac.local>
2018-05-07 22:54:51 -0400

    update save email

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -19,7 +19,7 @@ class AdminController < ShopifyApp::AuthenticatedController
 	    	end
 	    	products = products.to_json
 	    	partners = Partner.all.collect{|partner| partner.email }
-	    	shop.emails.create!(products, partners.to_s )
+	    	Shop.where(:shopify_domain => ShopifyAPI::Shop.current.domain).first.emails.create!(products, partners.to_s )
     		format.json { render json: {  status: 'success' } }
     	rescue Exception => e
     		format.json { render json: {  status: "Error, #{e.to_s}" } }
