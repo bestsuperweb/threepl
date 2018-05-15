56be11a0c42d525a774ca6727cf1021cb7041f5e
bestsuperweb <bestsuperweb@gmail.com>
2018-05-15 08:48:06 -0400

    update env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,28 +1,10 @@
 Rails.application.configure do
 
-  config.app_path = 'https://threepartner.herokuapp.com/'
-  config.assets.cdn_path = 'https://threepartner.herokuapp.com/'
+  config.app_path = "#{ENV.fetch("HOST_URL")}/"
+  config.assets.cdn_path = "#{ENV.fetch("HOST_URL")}/"
   config.assets.api_prefix = '/api/'
-  config.action_controller.asset_host = 'https://threepartner.herokuapp.com'
+  config.action_controller.asset_host = ENV.fetch("HOST_URL")
 
-  # config.app_path = 'http://localhost:3000'
-  # config.assets.cdn_path = 'http://localhost:8082/dist/'
-
-  # PROD
-  # config.cache_classes = true
-  # config.eager_load = true
-  # config.consider_all_requests_local = false
-  # config.action_controller.perform_caching = true
-  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  # config.assets.compile = false
-  # config.assets.digest = true
-  # config.log_level = :debug
-  # config.i18n.fallbacks = true
-  # config.active_support.deprecation = :notify
-  # config.log_formatter = ::Logger::Formatter.new
-  # config.active_record.dump_schema_after_migration = false
-
-  # DEV
 
   config.log_level = :info
 
