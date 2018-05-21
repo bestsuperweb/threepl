a5ef672fcfb50095572bf4180b1aceed0627d872
bestsuperweb <bestsuperweb@gmail.com>
2018-05-21 06:41:04 -0400

    remove mandrillmailer

--- app/mailers/send_mailer.rb
+++
@@ -1,8 +0,0 @@
-class SendMailer < BaseMandrillMailer
-	default from: ENV["FROM_EMAIL"]
-	def send_products(vars, email)
-		subject = "Products"
-		body = mandrill_template("products", vars)
-		send_mail(email, subject, body)
-	end
-end
