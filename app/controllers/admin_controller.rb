cf50c9fbef8190d1293f78b746ad020c3b76b774
bestsuperweb <bestsuperweb@gmail.com>
2018-05-25 08:47:49 -0400

    update cc destination

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,89 +3,89 @@ class AdminController < ShopifyApp::AuthenticatedController
 
   def index
     @products = ShopifyAPI::Product.all
-    begin
-    	template = SES.get_template({
-		  template_name: "3pl_quote_request", # required
-		})
-		if template.template
-			SES.update_template({
-			  template: { # required
-			    template_name: "3pl_quote_request", # required
-			    subject_part: "Products",
-			    text_part: 'Here are products: /n {{#each products}} {{id}} - {{title}}, {{weight}}, {{whl}}, {{battery}}, {{notes}} /n {{/each}}',
-			    html_part: 'Here are products: /n <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">
-	                			{{#each products}}
-	                            <tr >
-	                                <td valign=\"top\" >
-	                                <img src={{image}} width=\"50\"  alt={{title}}>
-	                                </td>
-	                                <td valign=\"top\"> {{id}}
-	                                </td>
-	                                <td valign=\"top\" >
-	                                {{title}}
-	                                </td>
-	                                <td valign=\"top\" >
-	                                {{weight}}
-	                                </td>
-	                                <td valign=\"top\" >
-	                                {{whl}}
-	                                </td>
-	                                <td valign=\"top\" >
-	                                {{battery}}
-	                                </td>
-	                                <td valign=\"top\" >
-	                                {{notes}}
-	                                </td>
-	                            </tr>
-	                            {{/each}}
-	                            </table>',
-			  },
-			})
-		else
-			SES.create_template({
-			  template: { # required
-			    template_name: "3pl_quote_request", # required
-			    subject_part: "Products",
-			    text_part: 'Here are products: /n {{#each products}} {{id}} - {{title}}, {{weight}}, {{whl}}, {{battery}}, {{notes}} /n {{/each}}',
-			    html_part: 'Here are products: /n <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">
-	                			{{#each products}}
-	                            <tr >
-	                                <td valign=\"top\" >
-	                                <img src={{image}} width=\"50\"  alt={{title}}>
-	                                </td>
-	                                <td valign=\"top\"> {{id}}
-	                                </td>
-	                                <td valign=\"top\" >
-	                                {{title}}
-	                                </td>
-	                                <td valign=\"top\" >
-	                                {{weight}}
-	                                </td>
-	                                <td valign=\"top\" >
-	                                {{whl}}
-	                                </td>
-	                                <td valign=\"top\" >
-	                                {{battery}}
-	                                </td>
-	                                <td valign=\"top\" >
-	                                {{notes}}
-	                                </td>
-	                            </tr>
-	                            {{/each}}
-	                            </table>',
-			  },
-			})
-		end
+  #   begin
+  #   	template = SES.get_template({
+		#   template_name: "3pl_quote_request", # required
+		# })
+		# if template.template
+		# 	SES.update_template({
+		# 	  template: { # required
+		# 	    template_name: "3pl_quote_request", # required
+		# 	    subject_part: "Products",
+		# 	    text_part: 'Here are products: /n {{#each products}} {{id}} - {{title}}, {{weight}}, {{whl}}, {{battery}}, {{notes}} /n {{/each}}',
+		# 	    html_part: 'Here are products: /n <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">
+	 #                			{{#each products}}
+	 #                            <tr >
+	 #                                <td valign=\"top\" >
+	 #                                <img src={{image}} width=\"50\"  alt={{title}}>
+	 #                                </td>
+	 #                                <td valign=\"top\"> {{id}}
+	 #                                </td>
+	 #                                <td valign=\"top\" >
+	 #                                {{title}}
+	 #                                </td>
+	 #                                <td valign=\"top\" >
+	 #                                {{weight}}
+	 #                                </td>
+	 #                                <td valign=\"top\" >
+	 #                                {{whl}}
+	 #                                </td>
+	 #                                <td valign=\"top\" >
+	 #                                {{battery}}
+	 #                                </td>
+	 #                                <td valign=\"top\" >
+	 #                                {{notes}}
+	 #                                </td>
+	 #                            </tr>
+	 #                            {{/each}}
+	 #                            </table>',
+		# 	  },
+		# 	})
+		# else
+		# 	SES.create_template({
+		# 	  template: { # required
+		# 	    template_name: "3pl_quote_request", # required
+		# 	    subject_part: "Products",
+		# 	    text_part: 'Here are products: /n {{#each products}} {{id}} - {{title}}, {{weight}}, {{whl}}, {{battery}}, {{notes}} /n {{/each}}',
+		# 	    html_part: 'Here are products: /n <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">
+	 #                			{{#each products}}
+	 #                            <tr >
+	 #                                <td valign=\"top\" >
+	 #                                <img src={{image}} width=\"50\"  alt={{title}}>
+	 #                                </td>
+	 #                                <td valign=\"top\"> {{id}}
+	 #                                </td>
+	 #                                <td valign=\"top\" >
+	 #                                {{title}}
+	 #                                </td>
+	 #                                <td valign=\"top\" >
+	 #                                {{weight}}
+	 #                                </td>
+	 #                                <td valign=\"top\" >
+	 #                                {{whl}}
+	 #                                </td>
+	 #                                <td valign=\"top\" >
+	 #                                {{battery}}
+	 #                                </td>
+	 #                                <td valign=\"top\" >
+	 #                                {{notes}}
+	 #                                </td>
+	 #                            </tr>
+	 #                            {{/each}}
+	 #                            </table>',
+		# 	  },
+		# 	})
+		# end
 
-    	template_data = { products: [{ id: '123123', image: 'https://cdn.shopify.com/s/files/1/2193/6543/products/product-image-375612158_42e298e9-fa63-4c06-83f2-ae062f8340de.jpg?v=1504059907', title: 'Blackbody', weight: '45lb', whl: '123 x 123 x 23', battery: 'No', notes: 'Special notes'}]}.to_json
-    	resp = SES.test_render_template({
-    		template_name: "3pl_quote_request", # required
-    		template_data: template_data.to_s # required
-    		})
-    	@response = resp.rendered_template
-    rescue Exception => e
-    	@response = e
-    end
+  #   	template_data = { products: [{ id: '123123', image: 'https://cdn.shopify.com/s/files/1/2193/6543/products/product-image-375612158_42e298e9-fa63-4c06-83f2-ae062f8340de.jpg?v=1504059907', title: 'Blackbody', weight: '45lb', whl: '123 x 123 x 23', battery: 'No', notes: 'Special notes'}]}.to_json
+  #   	resp = SES.test_render_template({
+  #   		template_name: "3pl_quote_request", # required
+  #   		template_data: template_data.to_s # required
+  #   		})
+  #   	@response = resp.rendered_template
+  #   rescue Exception => e
+  #   	@response = e
+  #   end
   end
 
   def send_eamils
@@ -98,7 +98,7 @@ class AdminController < ShopifyApp::AuthenticatedController
 			  source: ENV['FROM_EMAIL'], # required
 			  destination: { # required
 			    to_addresses: Partner.all.collect{|partner| partner.email },
-			    cc_addresses: 'stevejeandev@gmail.com'
+			    cc_addresses: ['stevejeandev@gmail.com']
 			  },
 			  tags: [
 			    {
