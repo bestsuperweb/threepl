59ed40c69f68ac721c657c9b428bf0eed45fd1b7
bestsuperweb <bestsuperweb@gmail.com>
2018-08-20 13:02:20 -0400

    update mailer for send grid

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,116 +3,19 @@ class AdminController < ShopifyApp::AuthenticatedController
 
   def index
     @products = ShopifyAPI::Product.all
-
-    # ------- Create template if no template
-  #   begin
-  #   	template = SES.get_template({
-		#   template_name: "3pl_quote_request", # required
-		# })
-		# if template.template
-		# 	SES.update_template({
-		# 	  template: { # required
-		# 	    template_name: "3pl_quote_request", # required
-		# 	    subject_part: "Products",
-		# 	    text_part: 'Here are products: /n {{#each products}} {{id}} - {{title}}, {{weight}}, {{whl}}, {{battery}}, {{notes}} /n {{/each}}',
-		# 	    html_part: 'Here are products: /n <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">
-	 #                			{{#each products}}
-	 #                            <tr >
-	 #                                <td valign=\"top\" >
-	 #                                <img src={{image}} width=\"50\"  alt={{title}}>
-	 #                                </td>
-	 #                                <td valign=\"top\"> {{id}}
-	 #                                </td>
-	 #                                <td valign=\"top\" >
-	 #                                {{title}}
-	 #                                </td>
-	 #                                <td valign=\"top\" >
-	 #                                {{weight}}
-	 #                                </td>
-	 #                                <td valign=\"top\" >
-	 #                                {{whl}}
-	 #                                </td>
-	 #                                <td valign=\"top\" >
-	 #                                {{battery}}
-	 #                                </td>
-	 #                                <td valign=\"top\" >
-	 #                                {{notes}}
-	 #                                </td>
-	 #                            </tr>
-	 #                            {{/each}}
-	 #                            </table>',
-		# 	  },
-		# 	})
-		# else
-		# 	SES.create_template({
-		# 	  template: { # required
-		# 	    template_name: "3pl_quote_request", # required
-		# 	    subject_part: "Products",
-		# 	    text_part: 'Here are products: /n {{#each products}} {{id}} - {{title}}, {{weight}}, {{whl}}, {{battery}}, {{notes}} /n {{/each}}',
-		# 	    html_part: 'Here are products: /n <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">
-	 #                			{{#each products}}
-	 #                            <tr >
-	 #                                <td valign=\"top\" >
-	 #                                <img src={{image}} width=\"50\"  alt={{title}}>
-	 #                                </td>
-	 #                                <td valign=\"top\"> {{id}}
-	 #                                </td>
-	 #                                <td valign=\"top\" >
-	 #                                {{title}}
-	 #                                </td>
-	 #                                <td valign=\"top\" >
-	 #                                {{weight}}
-	 #                                </td>
-	 #                                <td valign=\"top\" >
-	 #                                {{whl}}
-	 #                                </td>
-	 #                                <td valign=\"top\" >
-	 #                                {{battery}}
-	 #                                </td>
-	 #                                <td valign=\"top\" >
-	 #                                {{notes}}
-	 #                                </td>
-	 #                            </tr>
-	 #                            {{/each}}
-	 #                            </table>',
-		# 	  },
-		# 	})
-		# end
-
-  # ------ Get email sent history using SES
-  # @response = SES.get_send_statistics({})
-
   end
 
   def send_eamils
   	shop = Shop.where( shopify_domain: ShopifyAPI::Shop.current.domain ).first
     products = params[:products]
-    respond_to do |format|
-    	begin
-    		template_data = { products: products.collect{|p| p[1].to_json } }.to_json    	
-		    email = SES.send_templated_email({
-			  source: ENV['FROM_EMAIL'], # required
-			  destination: { # required
-			    to_addresses: Partner.all.collect{|partner| partner.email },
-			    cc_addresses: ['stevejeandev@gmail.com']
-			  },
-			  tags: [
-			    {
-			      name: "Shop", # required
-			      value: shop.shopify_domain.gsub(/./, '_'), # required
-			    },
-			  ],
-			  template: "3pl_quote_request", # required
-			  template_data: template_data.to_s # required
-			})
-
-	    	partners = Partner.all.collect{|partner| partner.email }
-	    	shop.emails.create!({products: products.collect{|p| p[1][:title]}.to_s, partners: "#{email.message_id} - #{partners.to_s}" })
-    		format.json { render json: {  status: 'success' } }
-    	rescue Exception => e
-    		format.json { render json: {  status: "Error, #{e.to_s}" } }
-    	end
-    end
+    Partner.all.each do |partner|
+      options = {
+        :partner  => partner,
+        :shop     => shop.domain,
+        :products => products
+      }
+      Adminmailer.send_email(options).deliver_now
+    end    
   end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,16 +19,19 @@ Rails.application.configure do
   config.assets.digest = true
   config.assets.raise_runtime_errors = true
 
+   # remove if no need
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'threepl.herokuapp.com' }
+  config.action_mailer.asset_host = 'https://threepl.herokuapp.com'
 
-  config.action_mailer.smtp_settings = {
-    :address => ENV["SMTP_ADDRESS"],
-    :port => 587,
-    :user_name => ENV["SMTP_USERNAME"], 
-    :password => ENV["SMTP_PASSWORD"], 
-    :authentication => :login,
-    :enable_starttls_auto => true    
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
   }
-  
+
 end
