4fd707ba2595f2bd801460ad28daeb919fe415da
bestsuperweb <bestsuperweb@gmail.com>
2018-08-21 05:51:00 -0400

    ...

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -27,10 +27,10 @@ class AdminController < ShopifyApp::AuthenticatedController
   private
 
   def send_email(options={})
-    @partner        = options[:partner]
-    @shop           = options[:shop]
-    @products       = options[:products]
-    @products_list  = "<table>
+    partner        = options[:partner]
+    shop           = options[:shop]
+    products       = options[:products]
+    products_list  = "<table>
                           <thead>
                             <tr>
                               <th>#</th>
@@ -42,8 +42,8 @@ class AdminController < ShopifyApp::AuthenticatedController
                               <th>Special Notes</th>
                             </tr>
                           </thead>"
-    @products.each_with_index do |product, index|
-      @products_list  = @products_list  + "<tr>
+    products.each_with_index do |product, index|
+      products_list  = products_list  + "<tr>
                                             <td>#{index + 1}</td>
                                             <td><img src=#{product[1][:image]} width=\"50\" ></td>
                                             <td>#{product[1][:title]}</td>
@@ -53,17 +53,17 @@ class AdminController < ShopifyApp::AuthenticatedController
                                             <td>#{product[1][:notes]}</td>
                                           </tr>"
     end
-    @products_list  = @products_list  + "</table>"
+    products_list  = @products_list  + "</table>"
     mail = Mail.new
-    mail.from = Email.new(email: @partner.email)
+    mail.from = Email.new(email: "bestsuperweb@gmail.com")
     mail.subject = "Products from #{@shop}"
     personalization = Personalization.new
     personalization.add_to(Email.new(email: @partner.email))
-    personalization.add_substitution(Substitution.new(key: '-shop-', value: @shop.to_s))
-    personalization.add_substitution(Substitution.new(key: '-products-', value: @products_list.to_s))
+    personalization.add_substitution(Substitution.new(key: '-shop-', value: shop.to_s))
+    personalization.add_substitution(Substitution.new(key: '-products-', value: products_list.to_s))
     mail.add_personalization(personalization)
-    mail.add_content(Content.new(type: 'text/html', value: "Products from #{@shop}" ))
-    mail.template_id = User.all.first.template
+    mail.add_content(Content.new(type: 'text/html', value: "Products from #{shop}" ))
+    mail.template_id = 'd-8f6b10d7d3944edd86592ff3beebf0fb'
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     begin
