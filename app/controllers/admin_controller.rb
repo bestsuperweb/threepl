dd1916cac400d4e316276016e5272bfb81f2df8d
bestsuperweb <bestsuperweb@gmail.com>
2018-08-21 11:31:42 -0400

    update mailer

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -30,78 +30,34 @@ class AdminController < ShopifyApp::AuthenticatedController
     partner        = options[:partner]
     shop           = options[:shop]
     products       = options[:products]
-    products_list  = "<table>
-                          <thead>
-                            <tr>
-                              <th>#</th>
-                              <th>Image</th>
-                              <th>Title</th>
-                              <th>Weight</th>
-                              <th>W x H x L</th>
-                              <th>Has Battery?</th>
-                              <th>Special Notes</th>
-                            </tr>
-                          </thead>"
-    products.each_with_index do |product, index|
-      products_list  = products_list  + "<tr>
-                                            <td>#{index + 1}</td>
-                                            <td><img src=#{product[1][:image]} width=\"50\" ></td>
-                                            <td>#{product[1][:title]}</td>
-                                            <td>#{product[1][:weight].html_safe}</td>
-                                            <td>#{product[1][:whl]}</td>
-                                            <td>#{product[1][:battery]}</td>
-                                            <td>#{product[1][:notes]}</td>
-                                          </tr>"
-    end
-    products_list  = products_list  + "</table>"
-    # mail = Mail.new
-    # mail.from = Email.new(email: "bestsuperweb@gmail.com")
-    # mail.subject = "Products from #{shop}"
-    # personalization = Personalization.new
-    # personalization.add_to(Email.new(email: partner.email))
-    # personalization.add_substitution(Substitution.new(key: '-shop-', value: "shop.to_s"))
-    # personalization.add_substitution(Substitution.new(key: '-products-', value: "products_list.to_s"))
-    # mail.add_personalization(personalization)
-    # mail.add_content(Content.new(type: 'text/html', value: "Products from #{shop}" ))
-    # mail.template_id = User.all.first.template
 
-    # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    # begin
-    #     response = sg.client.mail._("send").post(request_body: mail.to_json)
-    # rescue Exception => e
-    #     puts e.message
-    # end
-    # puts response.status_code
-    # puts response.body
-    # puts response.parsed_body
-    # puts response.headers
-
-    data = JSON.parse('{
-      "personalizations": [
+    data = JSON.parse("{
+      'personalizations': [
         {
-          "to": [
+          'to': [
             {
-              "email": "bestsuperweb@gmail.com"
+              'email': #{ENV['FROM_EMAIL']}
             }
           ],
-          "dynamic_template_data": {
-            "shop": "BestStore",
-            "products": "produccts list..."
+          'dynamic_template_data': {
+            'shop': #{shop}
+            'products': #{products.to_json}
           },
-          "subject": "I\'m replacing the subject tag"
+          'subject': 'subject'
         }
       ],
-      "from": {
-        "email": "test@example.com"
+      'from': {
+        'email': #{ENV['FROM_EMAIL']}
       },
-      "content": [
+      'subject': 'Products from #{shop}',      
+      'content': [
         {
-          "type": "text/html",
-          "value": "I\'m replacing the <strong>body tag</strong>"
+          'type': 'text/html',
+          'value': 'body'
         }
       ],
-      "template_id": "d-8f6b10d7d3944edd86592ff3beebf0fb"
-    }')
+      'template_id': #{User.all.first.template}
+    }")
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     begin
         response = sg.client.mail._("send").post(request_body: data)
