9bcd2e48950cbbee59d00769f9bdfaf2f3db4cbc
bestsuperweb <bestsuperweb@gmail.com>
2018-09-06 11:56:31 -0400

    add charge block to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 	'admin', 			to: 'partners#dashboard',		as: 'admin'
   post 	'send/emails', 		to: 'admin#send_eamils',		as: 'send_eamils'
   post 	'save/template', 	to: 'partners#save_template', 	as: 'save_template'
+  get 	'recurring/charge', to: 'admin#create_recurring_application_charge', as: 'create_recurring_charge'
+  get 	'activatecharge', 	to: 'admin#activatecharge', 	as: 'activate_charge'
   
   root :to => 'admin#index'
 end
