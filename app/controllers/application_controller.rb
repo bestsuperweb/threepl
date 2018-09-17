b3046d446a5a131272612ae4fb9e4a016f3edcab
bestsuperweb <bestsuperweb@gmail.com>
2018-09-16 22:35:08 -0400

    modify routes after devise login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,13 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
+
+  def after_sign_in_path_for(resource)
+	dashboard_path
+  end
+
+  def after_sign_up_path_for(resource)
+    new_user_session_path
+  end
+
 end
