3219575e5be1515408784f52d43cbebd2ec902af
bestsuperweb <bestsuperweb@gmail.com>
2018-09-16 22:39:18 -0400

    add routes for partner board

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+
+	# partner routes
+  get 	'home', 			to: 'static#home', 			as: 'home'
+  get 	'dashboard', 		to: 'static#dashboard', 	as: 'dashboard'
+  get 	'quote/:id/new',	to: 'static#new_quote', 	as: 'new_quote'
+  post 	'quote', 			to: 'static#create_quote', 	as: 'create_quote'
+  get 	'quote', 			to: 'static#show_quote', 	as: 'show_quote'
+  get 	'request', 			to: 'static#show_email',	as: 'show_request'
+  get 	'setting', 			to: 'static#setting', 		as: 'setting'
+
   resources :etemplates
   devise_for :users
-  resources :partners
   mount ShopifyApp::Engine, at: '/'
   get 	'admin', 			to: 'partners#dashboard',		as: 'admin'
   post 	'send/emails', 		to: 'admin#send_eamils',		as: 'send_eamils'
