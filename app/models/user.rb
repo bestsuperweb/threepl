1c573c5426099d9052995bfaa09dce9698bc6396
bestsuperweb <bestsuperweb@gmail.com>
2018-09-16 22:36:16 -0400

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         # :confirmable, :lockable
+
+  has_many :emails
+  has_many :quotes
+  
+  def send_devise_notification(notification, *args)
+	 devise_mailer.send(notification, self, *args).deliver_later
+  end
+
+  def admin?
+  	self.admin
+  end
+
 end
