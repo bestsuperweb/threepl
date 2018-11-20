6b0cd6fd6c1f17a3ac3d8b6662c90f2640af2153
bestsuperweb <bestsuperweb@gmail.com>
2018-11-19 21:47:39 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 	# partner routes
   get 	'home', 			to: 'static#home', 			as: 'home'
   get 	'dashboard', 		to: 'static#dashboard', 	as: 'dashboard'
-  get 	'requests', 		to: 'static#requests', 		as: 'requests'
+  get 	'emails',	 		to: 'static#emails', 		as: 'emails'
   get 	'quote/:id/new',	to: 'static#new_quote', 	as: 'new_quote'
   post 	'quote', 			to: 'static#create_quote', 	as: 'create_quote'
   get 	'quote', 			to: 'static#show_quote', 	as: 'show_quote'
-  get 	'request', 			to: 'static#show_email',	as: 'show_request'
+  get 	'email/:id',		to: 'static#show_email',	as: 'show_email'
   get 	'setting', 			to: 'static#setting', 		as: 'setting'
   post  'save/template', 	to: 'static#save_template', as: 'save_template'
 
