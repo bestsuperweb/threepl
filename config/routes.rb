c99e31ea96bd47f1d48b35759175f93707fcadd0
bestsuperweb <bestsuperweb@gmail.com>
2018-11-27 07:12:08 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   get 	'home', 			to: 'static#home', 			as: 'home'
   get 	'dashboard', 		to: 'static#dashboard', 	as: 'dashboard'
   get 	'emails',	 		to: 'static#emails', 		as: 'emails'
-  get 	'quote/:id/new',	to: 'static#new_quote', 	as: 'new_quote'
   post 	'quote', 			to: 'static#create_quote', 	as: 'create_quote'
-  get 	'quote', 			to: 'static#show_quote', 	as: 'show_quote'
   get 	'email/:id',		to: 'static#show_email',	as: 'show_email'
   get 	'setting', 			to: 'static#setting', 		as: 'setting'
   post  'save/template', 	to: 'static#save_template', as: 'save_template'
@@ -15,6 +13,8 @@ Rails.application.routes.draw do
   devise_for :users
   mount ShopifyApp::Engine, at: '/'
   post 	'send/emails', 		to: 'admin#send_emails',		as: 'send_emails'
+  get 	'admin/quotes', 	to: 'admin#quotes', 			as: 'show_quotes'
+  get 	'admin/email/:id', 	to: 'admin#email_details', 		as: 'email_details'
   get 	'recurring/charge', to: 'admin#create_recurring_application_charge', as: 'create_recurring_charge'
   get 	'activatecharge', 	to: 'admin#activatecharge', 	as: 'activate_charge'
   
